cbdaf3d59875da3216a4459fe1d6194fbae162be
nolbo <52912534+nolbo@users.noreply.github.com>
2022-10-18 07:57:38 +0900

    무한 반복에 걸리는 문제 수정

--- main2.c
+++ main2.c
@@ -10,13 +10,25 @@ int *list;
 int Partition(int l, int r)
 {
     int pivot = list[l];
-    int low = l + 1;
-    int high = r;
+    int low = l;
+    int high = r + 1;
+    
+    /*printf("left: %d right: %d low: %d high: %d\n", l, r, low, high);
+    for (int i = 0; i < n; i ++)
+    {
+        list[i] = rand() % n + 1;
+        printf("%d ", list[i]);
+    }
+    printf("\n");*/
 
     while (low < high)
     {
-        while (low <= r && list[low] < pivot) low ++;
-        while (high >= l && list[high] > pivot) high --;
+        do {
+            low ++;
+        } while (low <= r && list[low] < pivot);
+        do {
+            high --;
+        } while (high >= l && list[high] > pivot);
 
         if (low < high)
         {
@@ -53,17 +65,16 @@ int main()
     for (i = 0; i < n; i ++)
     {
         list[i] = rand() % n + 1;
-        printf("%d ", list[i]);
+        // printf("%d ", list[i]);
     }
     
     start = clock();
-    printf("start: %f\n", (float)start / CLOCKS_PER_SEC);
+    printf("\nstart: %f\n", (float)start / CLOCKS_PER_SEC);
     QuickSort(0, n - 1);
     end = clock();
 
     for (j = 0; j < n; j ++)
         printf("%d ", list[j]);
-    printf("\nb");
     printf("\nn = %d, time = %f\n", n, (float)(end - start) / CLOCKS_PER_SEC);
 
     free(list);
